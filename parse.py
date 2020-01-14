4f4e6a8fbe42a318fb2841ab84cabadf3e08fa7a
SerbanMatei <48119059+SerbanMatei@users.noreply.github.com>
2020-01-14 23:24:31 +0200

    Added recommendation method

--- parse.py
+++ parse.py
@@ -1,9 +1,9 @@
 import play_csv
 import string
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from gensim.models.word2vec import Word2Vec
-user_input = ['toy']
-words = ['The', 'flowers', 'are', 'beautiful']
+
+text = ['action movie will ferrell']
 movies = play_csv.play_csv()
 
 
@@ -15,45 +15,68 @@ def get_data_set():
         tags = movie.get('Tags')
 
         exclude = set(string.punctuation + string.digits)
-
         title = ''.join(ch for ch in title if ch not in exclude)
-        for i in word_tokenize(title):
-            data_set.append(i.lower())
+
+        for i in sent_tokenize(title):
+            temp = []
+            for j in word_tokenize(i):
+                temp.append(j.lower())
+            data_set.append(temp)
 
         for genre in genres:
-            for j in word_tokenize(genre):
-                data_set.append(j.lower())
+            for i in sent_tokenize(genre):
+                temp = []
+                for j in word_tokenize(i):
+                    temp.append(j.lower())
+                data_set.append(temp)
 
         for tag in tags:
             if tag is not None:
-                for k in word_tokenize(tag):
-                    data_set.append(k.lower())
+                for i in sent_tokenize(tag):
+                    temp = []
+                    for j in word_tokenize(i):
+                        temp.append(j.lower())
+                    data_set.append(temp)
     return data_set
 
 
-print(get_data_set())
-
-
-def parse(input):
+def parse(user_input):
     exclude = set(string.punctuation)
-    text = ''.join(ch for ch in input if ch not in exclude)
+    user_input = ''.join(ch for ch in user_input if ch not in exclude)
     temp = []
-    for i in word_tokenize(text):
+    for i in word_tokenize(user_input):
         temp.append(i.lower())
     return temp
 
 
-def get_similarities(text):
-    words = parse(text)
+def get_similarities(user_input):
+    words = parse(user_input)
     data_set = get_data_set()
     print('Start')
-    cbow_model = Word2Vec(data_set, min_count=1, size=1000, window=100)
+    cbow_model = Word2Vec(data_set, min_count=1, size=100, window=10)
     similarities = []
     for word in words:
         cbow_prediction = cbow_model.wv.most_similar(word, topn=1)
         syn = cbow_prediction[0][0]
         similarities.append(syn)
-        print(syn)
+    return similarities
 
 
-get_similarities(user_input)
+def get_recommendations(user_input):
+    movies_recommendations = []
+    similarities = get_similarities(user_input)
+    for word in similarities:
+        for movie in movies:
+            title = movie.get('Title')
+            genres = movie.get('Genre')
+            tags = movie.get('Tags')
+            if word in title or word in genres or word in tags:
+                movies_recommendations.append(movie)
+    sorted_movies = sorted(movies_recommendations, key=lambda j: j['Rating'], reverse=True)
+    top_movies = []
+    for i in range(10):
+        top_movies.append(sorted_movies[i])
+    print(top_movies)
+
+
+get_recommendations(text)
